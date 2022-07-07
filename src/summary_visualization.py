6a950df7d8ff54833d799fe8eeb0d730f4022bde
Burgon <alburgon2@gmail.com>
2022-07-07 17:14:54 -0400

    updated font and resolution

--- src/summary_visualization.py
+++ src/summary_visualization.py
@@ -1,6 +1,16 @@
 import seaborn
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 import pandas as pd
+
+# formatting
+colors = seaborn.color_palette("Paired")
+rcParams['font.family'] = 'monospace'
+rcParams['font.weight'] = 'bold'
+rcParams['font.size'] = 14
+hfont = {'fontname':'monospace', 'fontweight':'bold', 'fontsize':18}
+rcParams['figure.figsize'] = (8,6)
+f, ax = plt.subplots(figsize=(8,6))
 
 # read in dataframe
 RICORD_1c_json_path = "/gpfs_projects/alexis.burgon/OUT/2022_CXR/summary_table_RICORD_1c.json"
@@ -19,7 +29,6 @@ for row in df.itertuples():
         print(f"ERROR, got {row[4][0]['sex']} instead of F or M")
 # print(sex_counts)
 # create pie chart
-colors = seaborn.color_palette()
 plt.pie(sex_counts, labels=sex_labels, colors=colors, autopct='%.0f%%')
-plt.savefig(save_loc)
+plt.savefig(save_loc, dpi=300)
 
