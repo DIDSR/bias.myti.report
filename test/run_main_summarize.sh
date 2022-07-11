113a511dbb11949827d3fc665457476a15260f81
ravisamala <ravisamala@users.noreply.github.com>
2022-07-11 08:38:32 -0400

    []

--- test/run_main_summarize.sh
+++ test/run_main_summarize.sh
@@ -7,9 +7,9 @@
 EXE="../src/main_summarize.py"
 # #
 # declare -a IN_paths=("/home/ravi.samala/DATA/temp/open_AI_unzip/" "/home/ravi.samala/DATA/COVID_19_AR/manifest-1594658036421/COVID-19-AR/" "/home/ravi.samala/DATA/COVID_19_NY_SBU/manifest-1628608914773/COVID-19-NY-SBU/")
-declare -a IN_paths=("/gpfs_projects/ravi.samala/DATA/MIDRC2/open_AI_unzip/" "/home/ravi.samala/DATA/COVID_19_AR/manifest-1594658036421/COVID-19-AR/" "/home/ravi.samala/DATA/COVID_19_NY_SBU/manifest-1628608914773/COVID-19-NY-SBU/")
-declare -a IN_Repos=("open_AI" "COVID_19_AR" "COVID_19_NY_SBU")
-declare -a OUT_summary=("/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__open_AI.json" "/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__COVID_19_AR.tsv" "/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__COVID_19_NY_SBU2.tsv")
+declare -a IN_paths=("/gpfs_projects/ravi.samala/DATA/MIDRC2/open_RI_unzip/" "/gpfs_projects/ravi.samala/DATA/MIDRC2/open_AI_unzip/" "/home/ravi.samala/DATA/COVID_19_AR/manifest-1594658036421/COVID-19-AR/" "/home/ravi.samala/DATA/COVID_19_NY_SBU/manifest-1628608914773/COVID-19-NY-SBU/")
+declare -a IN_Repos=("open_RI" "open_AI" "COVID_19_AR" "COVID_19_NY_SBU")
+declare -a OUT_summary=("/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__open_RI.json" "/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__open_AI.json" "/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__COVID_19_AR.tsv" "/gpfs_projects/ravi.samala/OUT/2022_CXR/summary_table__COVID_19_NY_SBU2.tsv")
 # #
 # #
 # #
