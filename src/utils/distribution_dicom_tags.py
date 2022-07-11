32c19199159e2d4caacf2dc928c1b998f33c3b48
ravisamala <ravisamala@users.noreply.github.com>
2022-07-11 08:37:00 -0400

    savings counts of each key variable in json format

--- src/utils/distribution_dicom_tags.py
+++ src/utils/distribution_dicom_tags.py
@@ -47,20 +47,31 @@ def get_dist(in_dir, repo_name, dicom_tags, out_dir):
     for each_tag in dicom_tags:
         print(ds[each_tag].name.replace)
         out_fig_path_name = os.path.join(out_dir, repo_name + '__' + ds[each_tag].name.replace(' ', '_') + '.png')
-        pd.Series(tag_acc[each_tag]).value_counts(sort=False).plot(kind='bar')
+        out_json_path_name = os.path.join(out_dir, repo_name + '__' + ds[each_tag].name.replace(' ', '_') + '.json')
+        # pd.Series(tag_acc[each_tag]).value_counts(sort=False).plot(kind='bar')
+        hist_pd = pd.Series(tag_acc[each_tag]).value_counts(sort=True)
+        hist_pd.plot(kind='bar')
         plt.savefig(out_fig_path_name, dpi=300, bbox_inches="tight")
+        print(hist_pd)
+        # hist_df = hist_pd.rename_axis('unique_values').to_frame('counts')
+        hist_df = hist_pd.rename_axis('unique_values').reset_index(name='counts')
+        print(hist_df)
+        hist_df.to_json(out_json_path_name, indent=4, orient='table', index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='generate distribution of dicom tags')
     # parser.add_argument('-i', '--input_dir', help='Input dir', default='/home/ravi.samala/DATA/temp/open_AI_unzip/')
     # parser.add_argument('-i', '--input_dir', help='Input dir', default='/gpfs_projects/common_data/TCIA/COVID_19_AR/manifest-1594658036421/COVID-19-AR/')
-    parser.add_argument('-i', '--input_dir', help='Input dir', default='/gpfs_projects/common_data/TCIA/COVID_19_NY_SBU/manifest-1628608914773/COVID-19-NY-SBU/')
+    # parser.add_argument('-i', '--input_dir', help='Input dir', default='/gpfs_projects/common_data/TCIA/COVID_19_NY_SBU/manifest-1628608914773/COVID-19-NY-SBU/')
     # parser.add_argument('-i', '--input_dir', help='Input dir', default='/gpfs_projects/common_data/MIDRC/Release_1c/manifest-1610656454899/MIDRC-RICORD-1C')
-    parser.add_argument('-r', '--repo_name', help='name of the data repository', default='COVID-19-NY-SBU')
+    # parser.add_argument('-i', '--input_dir', help='Input dir', default='/gpfs_projects/ravi.samala/DATA/MIDRC2/open_AI_unzip/')
+    parser.add_argument('-i', '--input_dir', help='Input dir', default='/gpfs_projects/ravi.samala/DATA/MIDRC2/open_RI_unzip/')
+    parser.add_argument('-r', '--repo_name', help='name of the data repository', default='open_RI')
     parser.add_argument('-d', '--dicom_tags', action='append', help='List of dicom tags to track', default=[(0x0008, 0x1030), (0x0008, 0x0060)])
     parser.add_argument('-o', '--output_dir', action='append', help='Output dir to save figures', default='/gpfs_projects/ravi.samala/OUT/2022_CXR/')
     args = parser.parse_args()
     # # 
+    print(args.input_dir)
     get_dist(args.input_dir, args.repo_name, args.dicom_tags, args.output_dir)
 
