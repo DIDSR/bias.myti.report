2f41b5270c93bf6c65d431db926b4127f25417aa
ravisamala <ravisamala@users.noreply.github.com>
2022-11-30 09:53:52 -0500

    adding options for custom transfer learning

--- test/run_train.sh
+++ test/run_train.sh
@@ -8,28 +8,34 @@ IN_DIR="/nas/unas25/rsamala/2022_MAM_CADx_DCNN/RAND_sampling_experiments"
 NUM_EPOCHS=250
 BATCH_SIZE=64
 # #
+FINETUNE="partial"     # # "full" or "partial"
+FREEZE_UPTO=4       # # Works only for FINETUNE="partial"
+# #
 OPTIMIZER="sgd"
 START_LR=0.001
 # #
 NUM_THREADS=8
 SAVE_EVERY_N_EPOCHS=25
 # #
-MASTER_LOG="${IN_DIR}/master_log.log"
+MASTER_LOG="${IN_DIR}/master_log_custom_transfer_learning.log"
 # for ARCH in "googlenet" "resnet18" "wide_resnet50_2" "densenet121" "resnext50_32x4d"
-for ARCH in "wide_resnet50_2" "densenet121"
+for ARCH in "resnet18"
 do
-    # for RAND in 0 1 2 3 4 5 6 7 8 9
-    for RAND in 0 1 2 3 4 5 6 7 8 9
+    for FREEZE_UPTO in 0 1 2 3 4
     do
-        # for FOLD in 0 1 2 3
-        for FOLD in 0 1 2 3
+        # for RAND in 0 1 2 3 4 5 6 7 8 9
+        for RAND in 0
         do
-            IN_TR_LIST_FILE="${IN_DIR}/R${RAND}/f${FOLD}/tr.lis"
-            IN_VD_LIST_FILE="${IN_DIR}/R${RAND}/f${FOLD}/ts.lis"
-            OUT_DIR="${IN_DIR}/R${RAND}/f${FOLD}/${ARCH}/"
-            mkdir -p -- "$OUT_DIR"
-            # # Start the program
-            CUDA_LAUNCH_BLOCKING=1 python $EXE -i $IN_TR_LIST_FILE -v $IN_VD_LIST_FILE -o $OUT_DIR -d $ARCH -l $MASTER_LOG -n $NUM_EPOCHS -r $START_LR -b $BATCH_SIZE -p $OPTIMIZER -t $NUM_THREADS -e $SAVE_EVERY_N_EPOCHS
+            # for FOLD in 0 1 2 3
+            for FOLD in 1 2 3
+            do
+                IN_TR_LIST_FILE="${IN_DIR}/R${RAND}/f${FOLD}/tr.lis"
+                IN_VD_LIST_FILE="${IN_DIR}/R${RAND}/f${FOLD}/ts.lis"
+                OUT_DIR="${IN_DIR}/R${RAND}/f${FOLD}/${ARCH}_${FINETUNE}_${FREEZE_UPTO}/"
+                mkdir -p -- "$OUT_DIR"
+                # # Start the program
+                CUDA_LAUNCH_BLOCKING=1 python $EXE -i $IN_TR_LIST_FILE -v $IN_VD_LIST_FILE -o $OUT_DIR -d $ARCH -l $MASTER_LOG -n $NUM_EPOCHS -r $START_LR -b $BATCH_SIZE -p $OPTIMIZER -t $NUM_THREADS -e $SAVE_EVERY_N_EPOCHS -f $FINETUNE -u $FREEZE_UPTO
+            done
         done
     done
 done
