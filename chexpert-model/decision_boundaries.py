152632addc3645f9556bb0ecb5c5d7fedeee13ce
ravisamala <Ravi.Samala@fda.gov>
2023-01-28 18:37:29 -0500

    added save activation maps

--- chexpert-model/decision_boundaries.py
+++ chexpert-model/decision_boundaries.py
@@ -371,12 +371,15 @@ class DecisionBoundaryEvaluator():
                                 transform_args=transform_args,
                                 is_training=False,
                                 return_info_dict=True)
-            base_predictions, base_groundtruth, base_paths, base_last_dense_layer = predictor.predict(loader, return_embeddings=True)
+            base_predictions, base_groundtruth, base_paths, base_last_dense_layer, base_activation_maps = predictor.predict(loader, return_embeddings=True)
             if self.save_last_dense_layer:
                 # # save the embeddings
                 base_dense_layer_arrays = {'base_paths': base_paths, 'base_last_dense_layer': base_last_dense_layer, 'base_groundtruth': base_groundtruth}
+                base_activation_map_arrays = {'base_paths': base_paths, 'base_activation_maps': base_activation_maps, 'base_groundtruth': base_groundtruth}
                 embedding_npz_fp = os.path.join(self.experiment_dir, "all_orig__last_dense.npz")
                 np.savez_compressed(embedding_npz_fp, **base_dense_layer_arrays)
+                embedding_npz_fp = os.path.join(self.experiment_dir, "all_orig__activation_maps.npz")
+                np.savez_compressed(embedding_npz_fp, **base_activation_map_arrays)
             elif not self.ensemble: # TODO: integrate ensemble and embeddings
                 base_predictions, base_groundtruth, base_paths = predictor.predict(loader)
             else:
@@ -434,6 +437,7 @@ class DecisionBoundaryEvaluator():
             DB_arrays = {}
             if self.save_last_dense_layer:
                 DN_arrays = {}
+                ACT_arrays = {}
         for i in range(self.triplet_information[trip_idx]['samples'], self.max_samples):
             self.sample_df = sample_df
             print(f"{self.triplet_information[trip_idx]['triplet id']}: {i+1}/{self.max_samples}")
@@ -444,8 +448,10 @@ class DecisionBoundaryEvaluator():
                 planeloader = get_planeloader(data_args, dataframe=self.sample_df, img_idxs=current_sample, subgroups=self.input_classes, 
                                               prediction_tasks=model_args.tasks, **pl_setting)
                 if self.save_last_dense_layer:
-                    predictions, groundtruth, last_dense_layer = predictor.predict(planeloader, return_embeddings=True)
+                    # print('HERE')
+                    predictions, groundtruth, last_dense_layer, activation_maps = predictor.predict(planeloader, return_embeddings=True, hook_option=1)
                     DN_arrays[f"{i+1}__{current_sample[0]}_{current_sample[1]}_{current_sample[2]}"] = last_dense_layer
+                    ACT_arrays[f"{i+1}__{current_sample[0]}_{current_sample[1]}_{current_sample[2]}"] = activation_maps
                 elif not self.ensemble:
                     predictions, groundtruth = predictor.predict(planeloader)
                 else: 
@@ -471,6 +477,7 @@ class DecisionBoundaryEvaluator():
                 np.savez_compressed(self.triplet_information[trip_idx]['array fp'], **DB_arrays)
                 if self.save_last_dense_layer:
                     np.savez_compressed(self.triplet_information[trip_idx]['array fp'].replace('.npz', '__last_dense.npz'), **DN_arrays)
+                    np.savez_compressed(self.triplet_information[trip_idx]['array fp'].replace('.npz', '__activations.npz'), **ACT_arrays)
                 self.save()
             # return # DEBUG
 
