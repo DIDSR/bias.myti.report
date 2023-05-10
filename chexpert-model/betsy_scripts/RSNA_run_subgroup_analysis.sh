a325f8f2918560074b0f77da703b0689a9a40cf5
yuhangzhang0819 <Yuhang.Zhang@fda.hhs.gov>
2023-05-10 12:48:17 -0400

    code updated for subgroup bias measurement

---
+++ chexpert-model/betsy_scripts/RSNA_run_subgroup_analysis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+declare -a LAYER_ARRAY=("last_17" "last_6" "last_4" "last_3" "direct" "reverse_last_17" "reverse_last_6" "reverse_last_4" "reverse_last_3")
+BASE_WEIGHTS=CheXpert_Resnet
+EXPERIMENT_NAME=${BASE_WEIGHTS}_subgroup_size_decay_30_lr_5e-5
+TEST_SUBGROUP="sex"
+PRED_FILE=ensemble_by_patient_predictions.csv
+for BATCH in 5
+do
+for RAND in 0 1 2 3 4
+do
+MAIN_DIR=/scratch/yuhang.zhang/OUT/latent_space_run/batch_${BATCH}
+INFO_FILE=${MAIN_DIR}/validation_1_image.csv
+for RD in 0
+do
+for LAYER in ${LAYER_ARRAY[@]}
+do
+SUB_DIR=${MAIN_DIR}/RAND_${RAND}/target_model_${LAYER}_RD_${RD}/${EXPERIMENT_NAME}/
+PRED_DIR=${SUB_DIR}/results/
+python ../RSNA_subgroup_analysis.py  --rand ${RAND} \
+                                --sub_dir ${SUB_DIR} \
+                                --pred_dir ${PRED_DIR} \
+                                --test_subgroup ${TEST_SUBGROUP} \
+                                --prediction_file ${PRED_FILE} \
+                                --info_file ${INFO_FILE} \
+                                --post_processed False
+done
+done
+done
+done
