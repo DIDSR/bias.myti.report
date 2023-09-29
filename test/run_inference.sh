00a53e9fa8beaf0514e5027990a1b7110b12079a
yuhangzhang0819 <Yuhang.Zhang@fda.hhs.gov>
2023-09-29 14:16:03 -0400

    Update base model train and inference shell scripts.

--- test/run_inference.sh
+++ test/run_inference.sh
@@ -2,4 +2,9 @@
 # # running with the following venv
 # # source /gpfs_projects/ravi.samala/venvs/venv_Py310/bin/activate
 # #
-python ../src/myinference.py -i /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/subgroup_size/50w50b/RAND_0/train.csv -w /gpfs_projects/ravi.samala/OUT/2022_CXR/temp/pytorch_last_epoch_model.onnx -d CheXpert_Resnet -l /gpfs_projects/ravi.samala/OUT/2022_CXR/temp/inference_log.log
+python ../src/myinference.py  -i /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/validation_1_image.csv \
+                              -w /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/RAND_0/dcnn_test_last_17/pytorch_last_epoch_model.onnx \
+                              -d CheXpert_Resnet \
+                              -b 48 \
+                              -g 0 \
+                              -l /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/RAND_0/dcnn_test_last_17/inference_log.log
--- test/run_train.sh
+++ test/run_train.sh
@@ -2,4 +2,18 @@
 # # running with the following venv
 # # source /gpfs_projects/ravi.samala/venvs/venv_Py310/bin/activate
 # #
-python ../src/mytrain.py -i /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/subgroup_size/50w50b/RAND_0/train.csv -v /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/subgroup_size/50w50b/RAND_0/train.csv -o /gpfs_projects/ravi.samala/OUT/2022_CXR/temp/ -d CheXpert_Resnet -l /gpfs_projects/ravi.samala/OUT/2022_CXR/temp/run_log.log -p adam -g 0 -t 8 -e 1 -n 1
+python ../src/mytrain.py -i /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/RAND_0/train.csv \
+                         -v /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/RAND_0/validation.csv \
+                         -o /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/RAND_0/dcnn_test_last_17/ \
+                         -d CheXpert_Resnet \
+                         -l /gpfs_projects/yuhang.zhang/OUT/2022_CXR/bias/latent_space/batch_1/RAND_0/dcnn_test_last_17/run_log.log \
+                         -p adam \
+                         -b 48 \
+                         -g 6 \
+                         -t 8 \
+                         -e 1 \
+                         -n 15 \
+                         -r 5e-5 \
+                         --decay_multiplier 0.5 \
+                         --decay_every_N_epoch 5 \
+                         --bsave_valid_results_at_epochs True
