bbccfe7e47ef05f529532ef8ba5040d08851a424
yuhangzhang0819 <Yuhang.Zhang@fda.hhs.gov>
2023-10-05 15:45:11 -0400

    Modified data split script to sample data with different prevalence in each subgroup.

--- src/csv_data_split.py
+++ src/csv_data_split.py
@@ -3,50 +3,44 @@ import os
 import argparse
 from sklearn.utils import shuffle
 
-custom_subgroups ={ 
-    'sex':{'F','M'},
-    'race':{'Black', 'White'},
-}
 
-def train_split():
-    """
-    split the training/validation dataset evenly for each subgroup (sex, race and COVID)
+def train_split(args):
+    """ Split the training/validation dataset according to fraction.
     """
     #read input csv files
     train = pd.read_csv(os.path.join(args.in_dir, args.input_file))
     train_1_out = pd.DataFrame()
     train_2_out = pd.DataFrame()
+    test_subgroup = args.test_subgroup
+    frac = args.fraction
+    print(f"Start data split of {frac} and {1-frac} for {test_subgroup[0]} and {test_subgroup[1]}\n")
     #concat data splitted by the input fraction in each subgroup
-    keysList = list(custom_subgroups.keys())
-    grps = list(custom_subgroups[args.test_subgroup])
-    remain_key = [keys for keys in keysList if keys != args.test_subgroup]
-    grps_remain = list(custom_subgroups[remain_key[0]])
+    subgroup_list = ["F", "M", "Black", "White"]
+    remain_subgroup = list(set(subgroup_list) - set(test_subgroup))
     for i in range(2):
       for j in range(2):
         for k in range (2):
-          tr_temp = train[(train[grps[0]] == i)&(train[grps_remain[0]] == j)&(train['Yes'] == k)]
+          tr_temp = train[(train[test_subgroup[0]] == i)&(train[remain_subgroup[0]] == j)&(train['Yes'] == k)]
           if i == k:
-            tr_temp_1 = tr_temp.sample(frac=args.fraction)
+            tr_temp_1 = tr_temp.sample(frac=frac)
           else:
-            tr_temp_1 = tr_temp.sample(frac=1-args.fraction)
+            tr_temp_1 = tr_temp.sample(frac=1-frac)
           tr_temp_2 = tr_temp.drop(tr_temp_1.index)
           train_1_out = train_1_out.append(tr_temp_1)
           train_2_out = train_2_out.append(tr_temp_2)       
     #output 2 csv files     
     train_1_out = shuffle(train_1_out)
-    train_1_out.to_csv(os.path.join(args.save_dir, args.output_1), index=False)
+    train_1_out.to_csv(os.path.join(args.save_dir, f"{round(frac*100)}{test_subgroup[0][0]}P_{args.input_file}"), index=False)
     train_2_out = shuffle(train_2_out)
-    train_2_out.to_csv(os.path.join(args.save_dir, args.output_2), index=False)
+    train_2_out.to_csv(os.path.join(args.save_dir, f"{round((1-frac)*100)}{test_subgroup[0][0]}P_{args.input_file}"), index=False)
             
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--fraction',default=0.5,type=float)
-    parser.add_argument('--test_subgroup',type=str)
+    parser.add_argument('--test_subgroup',nargs=2,type=str,help="Current choices: F M or Black White")
     parser.add_argument('--in_dir',type=str)
     parser.add_argument('--save_dir',type=str)
     parser.add_argument('--input_file',type=str)
-    parser.add_argument('--output_1',type=str)
-    parser.add_argument('--output_2',type=str)
     args = parser.parse_args()
-    train_split()
+    train_split(args)
--- test/run_csv_data_split.sh
+++ test/run_csv_data_split.sh
@@ -5,22 +5,21 @@
 
 #NOTES: currently automatically split based on sex, race and COVID
 INPUT_FILE="train.csv"
-OUTOUT_1="train_50FP.csv"
-OUTOUT_2="train_50FP.csv"
+declare -a FRACTION_ARRAY=('0.5' '0.75' '0.9' '1')
 FRACTION=0.5 # valid range from 0 to 1
-TEST_SUB="sex"
 for BATCH in 0
 do
-for RAND in 1
+for RAND in 0
+do
+for FRACTION in ${FRACTION_ARRAY[@]}
 do
 IN_DIR=/scratch/yuhang.zhang/OUT/temp/batch_${BATCH}/RAND_${RAND}
 SAVE_DIR=/scratch/yuhang.zhang/OUT/temp/batch_${BATCH}/RAND_${RAND}
 python ../src/csv_data_split.py --fraction ${FRACTION} \
-                             --test_subgroup ${TEST_SUB} \
-                             --in_dir ${IN_DIR} \
-                             --save_dir ${SAVE_DIR} \
-                             --input_file ${INPUT_FILE} \
-                             --output_1 ${OUTOUT_1} \
-                             --output_2 ${OUTOUT_2}
+                                --test_subgroup Black White \
+                                --in_dir ${IN_DIR} \
+                                --save_dir ${SAVE_DIR} \
+                                --input_file ${INPUT_FILE}
+done
 done
 done
