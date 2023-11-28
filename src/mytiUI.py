b5f58d1c07cb517e264e083c5948026ca2ac5354
yuhangzhang0819 <Yuhang.Zhang@fda.hhs.gov>
2023-11-28 18:36:37 -0500

    Add file upload and figure save options. Add more descriptions about the experiments. Add features for plot selection, column selection, etc.

--- src/mytiUI.py
+++ src/mytiUI.py
@@ -1,75 +1,116 @@
 """
 Main design script for RST related to bias project
 """
-
+import pandas as pd
 from PyQt6.QtWidgets import * 
-from PyQt6 import QtCore 
+from PyQt6.QtCore import *
 from PyQt6.QtGui import * 
 from PyQt6.QtGui import QPixmap
 import sys 
 
-
+csv_path = '../example/example.csv'
+exp_type = 'Direct Approach'
+sample_size = False
+miti_compare = False
 class InitialPage(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.setWindowTitle('Myti.Report')
         self.UIComponents()
 
 
     def UIComponents(self):
-
-        #self.setGeometry(200, 200, 800, 600)
-        self.setWindowTitle('Myti.Report')
-        
+    
         # # csv file upload part
         # #
         # # file indicating label
-        lbl_up_load_file = QLabel('Uploaded Input File', self)
-        #lbl_up_load_file.resize(30, 100)
-        lbl_up_load_file.move(100, 55)
+        self.lbl_up_load_file = QLabel('Uploaded Input File', self)
+        self.lbl_up_load_file.move(100, 55)
         
-        # # edit box for file directory
-        edit_up_load_file = QLineEdit('testing', self)
-        edit_up_load_file.resize(350,25)
-        edit_up_load_file.move(240,50)
+        # # edit box for file directory   
+        self.edit_up_load_file = QLineEdit('testing', self)     
+        self.edit_up_load_file.resize(350,25)
+        self.edit_up_load_file.move(240,50)
 
-        # # button to browse the csv file
-        btn_up_load_file = QPushButton('Browse', self)
-        btn_up_load_file.setToolTip('Select <b>CSV File</b> from folder')
-        btn_up_load_file.resize(btn_up_load_file.sizeHint())
-        btn_up_load_file.move(620, 50)
+        # # button to browse the csv file  
+        self.btn_up_load_file = QPushButton('Browse', self)      
+        self.btn_up_load_file.setToolTip('Select <b>CSV File</b> from folder')
+        self.btn_up_load_file.resize(self.btn_up_load_file.sizeHint())
+        self.btn_up_load_file.move(620, 50)
+        self.btn_up_load_file.clicked.connect(self.upload_csv)
         
         # # experiment setting part
         # #
+        # # Experiment explanation 
+        self.lbl_exp_title = QLabel('Description of the approach', self)       
+        self.lbl_exp_title.move(400, 120)
+        self.lbl_exp_dscp = QLabel('Please select an experiment type.', self)
+        self.lbl_exp_dscp.setStyleSheet("border : 2px solid black;") 
+        self.lbl_exp_dscp.setGeometry(400, 160, 300, 250)
+        self.lbl_exp_dscp.setWordWrap(True) 
         # # experimeny type indicating label
-        lbl_exp_type = QLabel('Select Experiment Type', self)
-        lbl_exp_type.move(100, 200)
-        # # direct or indirect selection menu
-        combo_exp_type = QComboBox(self)
-        combo_exp_type.addItem('Direct Approach')
-        combo_exp_type.addItem('Indirect Approach')
-        combo_exp_type.move(100, 250)
-        # # finite sample size checkbox
-        cb_sample_size = QCheckBox('Finite Sample Size', self)
-        cb_sample_size.move(100, 300)
+        self.lbl_exp_type = QLabel('Select Experiment Type', self)        
+        self.lbl_exp_type.move(100, 120)
+        # # direct or indirect selection menu  
+        self.combo_exp_type = QComboBox(self)      
+        self.combo_exp_type.addItem('-Please Select-')
+        self.combo_exp_type.addItems(['Direct Approach','Indirect Approach'])
+        self.combo_exp_type.move(100, 170)
+        self.combo_exp_type.currentTextChanged.connect(self.approach_type)
+        # # finite sample size checkbox          
+        self.cb_sample_size = QCheckBox('Finite Sample Size', self)      
+        self.cb_sample_size.move(100, 220)
+        self.cb_sample_size.toggled.connect(self.sample_size_check) 
+        sample_size_info = 'If finite sample size is selected, results using different training dataset size will be computed and visualized.'
+        self.lbl_sample_size = QLabel(sample_size_info, self)
+        self.lbl_sample_size.setGeometry(100, 240, 250, 100)
+        self.lbl_sample_size.setWordWrap(True) 
         # # mitigation method compare checkbox
-        cb_myti_compare = QCheckBox('Compare Bias Mitigation Methods', self)
-        cb_myti_compare.move(100, 350)
-                
-        # # Experiment explanation
-        # #
-        lbl_exp_title = QLabel('Description of the approach', self)
-        lbl_exp_title.move(500, 200)
+        self.cb_miti_compare = QCheckBox('Compare Bias Mitigation Methods', self)        
+        self.cb_miti_compare.move(100, 330)
+        self.cb_miti_compare.toggled.connect(self.miti_compare_check) 
+        miti_compare_info = 'If comparing bias mitigation methods is selected, results using different bias mitigation methods will be campared and visualized.'
+        self.lbl_miti_compare = QLabel(sample_size_info, self)
+        self.lbl_miti_compare.setGeometry(100, 350, 250, 100)
+        self.lbl_miti_compare.setWordWrap(True) 
+                        
+        # # button for next page    
+        self.btn_next_page = QPushButton('Next Page', self)    
+        self.btn_next_page.resize(self.btn_next_page.sizeHint())
+        self.btn_next_page.move(600, 550)
+        self.btn_next_page.clicked.connect(self.next_page)
+
+    def upload_csv(self):
+        dialog = QFileDialog()
+        fname = dialog.getOpenFileName(None, "Import CSV", "", "CSV data files (*.csv)")
+        self.edit_up_load_file.setText(fname[0])
+        global csv_path
+        csv_path = fname[0]
         
-        # # button for next page
-        btn_next_page = QPushButton('Next Page', self)
-        btn_next_page.resize(btn_next_page.sizeHint())
-        btn_next_page.move(600, 500)
-        btn_next_page.clicked.connect(self.on_btn_next_page_clicked)
-
-
-    def on_btn_next_page_clicked(self):
+    def approach_type(self):
+        text = str(self.combo_exp_type.currentText())
+        global exp_type
+        exp_type = text 
+        if text == 'Direct Approach':
+            info = 'Direct approach applies data selection prior to training so that the disease prevalence is different for different patient subgroups.\n' + \
+            '\nThe degree to which bias is promoted can be controlled by changing the degree of prevalence gap between subgroups.'
+        elif text == 'Indirect Approach':
+            info = 'Indirect approach applies an extra step of transfer learning prior to target model fine-tuning to promote spurious correlation between model task and subgroup infomation.\n' + \
+            '\nThe degree to which bias is promoted can be controlled by number of frozen layers in last step fine-tuning.'
+        else:
+            info = 'Please select an experiment type.'       
+        self.lbl_exp_dscp.setText(info)
+    
+    def sample_size_check(self):
+        global sample_size
+        sample_size = True if self.cb_sample_size.isChecked() else False
+        
+    def miti_compare_check(self):
+        global miti_compare
+        miti_compare = True if self.cb_miti_compare.isChecked() else False        
+    
+    def next_page(self):
         widget.setCurrentWidget(secondpage)
 
 
@@ -77,126 +118,170 @@ class SecondPage(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.setWindowTitle('CSV Information')
         self.UIComponents()
         
     def UIComponents(self):
-
-        #self.setGeometry(200, 200, 800, 600)
-        self.setWindowTitle('CSV Information')
-        
+       
         # # Title Label
-        lbl_title = QLabel('Indicate Columns', self)
-        lbl_title.move(100, 50)
+        self.lbl_title = QLabel('Indicate Columns', self)
+        self.lbl_title.move(100, 50)
         
-        # # patient id selection
-        lbl_patient_id = QLabel('Patient Identifier:', self)
-        lbl_patient_id.move(100, 100)
-        combo_patient_id = QComboBox(self)
-        combo_patient_id.addItem('patient_id')
-        combo_patient_id.move(300, 100)
+        column_list = self.get_columns()
         # # subgroup label selection
-        lbl_subgroup = QLabel('Subgroup Label:', self)
-        lbl_subgroup.move(100, 150)
-        combo_subgroup = QComboBox(self)
-        combo_subgroup.addItem('patient_sex')
-        combo_subgroup.move(300, 150)
+        self.lbl_subgroup = QLabel('Subgroup Label:', self)
+        self.lbl_subgroup.move(100, 100)
+        self.combo_subgroup = QComboBox(self)
+        self.combo_subgroup.addItem('patient_sex')
+        self.combo_subgroup.addItems(column_list)
+        self.combo_subgroup.move(300, 100)
         # # output score selection
-        lbl_output = QLabel('Model Output:', self)
-        lbl_output.move(100, 200)
-        combo_output = QComboBox(self)
-        combo_output.addItem('output_socre')
-        combo_output.move(300, 200)
+        self.lbl_output = QLabel('Model Output:', self)
+        self.lbl_output.move(100, 150)
+        self.combo_output = QComboBox(self)
+        self.combo_output.addItem('output_socre')
+        self.combo_output.addItems(column_list)
+        self.combo_output.move(300, 150)
         # # actual label selection
-        lbl_label = QLabel('Sample Label:', self)
-        lbl_label.move(100, 250)
-        combo_label = QComboBox(self)
-        combo_label.addItem('actual_class')
-        combo_label.move(300, 250)
+        self.lbl_label = QLabel('Sample Label:', self)
+        self.lbl_label.move(100, 200)
+        self.combo_label = QComboBox(self)
+        self.combo_label.addItem('actual_class')
+        self.combo_label.addItems(column_list)
+        self.combo_label.move(300, 200)
         # # experimeny type selection
-        lbl_exp = QLabel('Experiment:', self)
-        lbl_exp.move(100, 300)
-        combo_exp = QComboBox(self)
-        combo_exp.addItem('100_female_prev')
-        combo_exp.move(300, 300)
+        self.lbl_exp = QLabel('Experiment:', self)
+        self.lbl_exp.move(100, 250)
+        self.combo_exp = QComboBox(self)
+        self.combo_exp.addItem('100_female_prev')
+        self.combo_exp.addItems(column_list)
+        self.combo_exp.move(300, 250)
         
         # # button to the previous page
-        btn_prev_page = QPushButton('Previous Page', self)
-        btn_prev_page.resize(btn_prev_page.sizeHint())
-        btn_prev_page.move(500, 500)
-        btn_prev_page.clicked.connect(self.on_btn_prev_page_clicked)
+        self.btn_prev_page = QPushButton('Previous Page', self)
+        self.btn_prev_page.resize(self.btn_prev_page.sizeHint())
+        self.btn_prev_page.move(500, 550)
+        self.btn_prev_page.clicked.connect(self.prev_page)
         # # button to the next page
-        btn_next_page = QPushButton('Next Page', self)
-        btn_next_page.resize(btn_next_page.sizeHint())
-        btn_next_page.move(600, 500)
-        btn_next_page.clicked.connect(self.on_btn_next_page_clicked)
+        self.btn_next_page = QPushButton('Next Page', self)
+        self.btn_next_page.resize(self.btn_next_page.sizeHint())
+        self.btn_next_page.move(600, 550)
+        self.btn_next_page.clicked.connect(self.next_page)
 
+    def get_columns(self):
+        data = pd.read_csv(csv_path)
+        return list(data.columns)
 
-    def on_btn_next_page_clicked(self):
+    def next_page(self):
         widget.setCurrentWidget(thirdpage)
         
-    def on_btn_prev_page_clicked(self):
+    def prev_page(self):
         widget.setCurrentWidget(firstpage)
         
 class FinalPage(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.setWindowTitle('Myti Results')
         self.UIComponents()
         
+        
     def UIComponents(self):
-
-        #self.setGeometry(200, 200, 800, 600)
-        self.setWindowTitle('Myti Results')
         
-        lbl_option = QLabel('Plot Options', self)
-        lbl_option.move(100, 50)
-        
-        pixmap = QPixmap('image.png')
+        # # title for plot selection
+        self.lbl_option = QLabel('Plot Options', self)
+        self.lbl_option.move(100, 20)
  
-        # adding example image
-        fig_1_label = QLabel(self)
-        pixmap_1 = QPixmap('../example/example_1.png').scaled(200,160)
-        fig_1_label.setPixmap(pixmap_1)
-        fig_1_label.resize(200,160)
-        fig_1_label.move(100,70)
+        # # adding example image
+        self.fig_1_label = QLabel(self)
+        self.pixmap_1 = QPixmap('../example/example_1.png')
+        self.fig_1_label.setPixmap(self.pixmap_1.scaled(200,150))
+        self.fig_1_label.resize(200,150)
+        self.fig_1_label.move(100,40)        
+        self.fig_2_label = QLabel(self)
+        self.pixmap_2 = QPixmap('../example/example_2.png')
+        self.fig_2_label.setPixmap(self.pixmap_2.scaled(200,150))
+        self.fig_2_label.resize(200,150)
+        self.fig_2_label.move(100,210)
         
-        fig_2_label = QLabel(self)
-        pixmap_2 = QPixmap('../example/example_2.png').scaled(200,160)
-        fig_2_label.setPixmap(pixmap_2)
-        fig_2_label.resize(200,160)
-        fig_2_label.move(100,240)
+        self.fig_3_label = QLabel(self)
+        self.pixmap_3 = QPixmap('../example/example_3.png')
+        self.fig_3_label.setPixmap(self.pixmap_3.scaled(200,150))
+        self.fig_3_label.resize(200,150)
+        self.fig_3_label.move(100,380)
+                
+        # # title for displaying plot
+        self.lbl_plot = QLabel('Selected Plot', self)
+        self.lbl_plot.move(350, 20)
+        # # title for plot discription
+        self.lbl_dscp = QLabel('Plot Description', self)
+        self.lbl_dscp.move(350, 320)
+        # # position for selected plot
+        self.lbl_selected_plot = QLabel(self)
+        self.lbl_selected_plot.resize(360,270)
+        self.lbl_selected_plot.move(350,40)
+        # # description for the plot
+        self.lbl_selected_dscp = QLabel(self)
+        self.lbl_selected_dscp.setStyleSheet("border : 2px solid black;")
+        self.lbl_selected_dscp.resize(360,200)
+        self.lbl_selected_dscp.move(350,340)
+        self.lbl_selected_dscp.setWordWrap(True) 
+        # # button to save figure
+        self.btn_save_fig = QPushButton('Save Figure', self)
+        self.btn_save_fig.resize(self.btn_save_fig.sizeHint())
+        self.btn_save_fig.move(600, 15)
+        self.btn_save_fig.clicked.connect(self.save_fig)
         
-        fig_3_label = QLabel(self)
-        pixmap_3 = QPixmap('../example/example_3.png').scaled(200,160)
-        fig_3_label.setPixmap(pixmap_3)
-        fig_3_label.resize(200,160)
-        fig_3_label.move(100,410)
-        
-        
-        lbl_plot = QLabel('Selected Plot', self)
-        lbl_plot.move(400, 50)
-        
-        lbl_dscp = QLabel('Plot Description', self)
-        lbl_dscp.move(400, 300)
-        
-        btn_save_fig = QPushButton('Save Figure', self)
-        btn_save_fig.resize(btn_save_fig.sizeHint())
-        btn_save_fig.move(600, 50)
-        
-        btn_prev_page = QPushButton('Previous Page', self)
-        btn_prev_page.resize(btn_prev_page.sizeHint())
-        btn_prev_page.move(500, 500)
-        btn_prev_page.clicked.connect(self.on_btn_prev_page_clicked)
-        
-        btn_quit_page = QPushButton('Quit', self)
-        btn_quit_page.resize(btn_quit_page.sizeHint())
-        btn_quit_page.move(600, 500)
-        btn_quit_page.clicked.connect(QApplication.instance().quit)
+        # # events when clicking on plot options        
+        self.fig_1_label.mousePressEvent = self.fig_1_click
+        self.fig_2_label.mousePressEvent = self.fig_2_click
+        self.fig_3_label.mousePressEvent = self.fig_3_click
+        # # button to the previous page
+        self.btn_prev_page = QPushButton('Previous Page', self)
+        self.btn_prev_page.resize(self.btn_prev_page.sizeHint())
+        self.btn_prev_page.move(500, 550)
+        self.btn_prev_page.clicked.connect(self.prev_page)
+        # # button to quit
+        self.btn_quit_page = QPushButton('Quit', self)
+        self.btn_quit_page.resize(self.btn_quit_page.sizeHint())
+        self.btn_quit_page.move(600, 550)
+        self.btn_quit_page.clicked.connect(QApplication.instance().quit)
 
-    def on_btn_prev_page_clicked(self):
+
+    def fig_1_click(self, event):
+        self.fig_1_label.setStyleSheet("border : 4px solid Green;")
+        self.fig_2_label.setStyleSheet("border : 0px solid Black;")
+        self.fig_3_label.setStyleSheet("border : 0px solid Black;")
+        self.lbl_selected_plot.setPixmap(self.pixmap_1.scaled(360,270))
+        info = 'Example description here. Example description here. Example description here. Example description here. Example description here. Example description here.'
+        self.lbl_selected_dscp.setText(info)       
+        
+    def fig_2_click(self, event):
+        self.fig_2_label.setStyleSheet("border : 4px solid Green;")
+        self.fig_1_label.setStyleSheet("border : 0px solid Black;")
+        self.fig_3_label.setStyleSheet("border : 0px solid Black;") 
+        self.lbl_selected_plot.setPixmap(self.pixmap_2.scaled(360,270))
+        info = 'Example description here. Example description here. Example description here. Example description here. Example description here. Example description here.'
+        self.lbl_selected_dscp.setText(info)   
+        
+    def fig_3_click(self, event):
+        self.fig_3_label.setStyleSheet("border : 4px solid Green;")
+        self.fig_2_label.setStyleSheet("border : 0px solid Black;")
+        self.fig_1_label.setStyleSheet("border : 0px solid Black;")
+        self.lbl_selected_plot.setPixmap(self.pixmap_3.scaled(360,270))
+        info = 'Example description here. Example description here. Example description here. Example description here. Example description here. Example description here.'
+        self.lbl_selected_dscp.setText(info)
+        
+    def prev_page(self):
         widget.setCurrentWidget(secondpage)
+        print(csv_path)
+    
+    def save_fig(self):
+        name = QFileDialog.getSaveFileName(self, 'Save File',"PNG (*.png)")
+        rect = QRect(300,40,720,650)
+        screen = self.grab()
+        figure = screen.copy(rect)
+        figure.save(name[0], 'png')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
