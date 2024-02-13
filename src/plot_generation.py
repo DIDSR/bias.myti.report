788c707c4336d529a9f30b2cf42bf7bae7387220
yuhangzhang0819 <Yuhang.Zhang@fda.hhs.gov>
2024-02-13 17:02:15 -0500

    Modifying plot code. Removing hard code.

--- src/plot_generation.py
+++ src/plot_generation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import pandas as pd
 from datetime import date
+from math import ceil
 import os
 
 # Formatting details
@@ -30,39 +31,27 @@ rcParams['xtick.color'] = AXIS_COLOR
 rcParams['ytick.color'] = AXIS_COLOR
 rcParams['axes.labelcolor'] = AXIS_COLOR
 rcParams['axes.edgecolor'] = AXIS_COLOR
-
-def plot_wrapper(plot_function, width_portion=1, aspect=0.75, **kwargs):
-    fig = plt.figure(figsize = (text_width*width_portion, text_width*width_portion*aspect))
-    gs, gs_kwargs = FUNCTION_DICT[plot_function](fig, **kwargs)
-    gs.tight_layout(fig, **gs_kwargs)
-    return fig
-
-
-def color_style_sampler(fig):
-    gs = fig.add_gridspec(1,1)
-    ax = fig.add_subplot(gs[0,0])
-    for i, (g, c) in enumerate(COLORS.items()):
-        ax.fill_between([0,1], [i-0.1, i-0.1], [i+0.1, i+0.1], color=c, alpha=CI_ALPHA)
-        ax.plot([0,1], [i-0.05, i-0.05], c=c)
-        ax.plot([0,1], [i+0.05, i+0.05], c=c, ls = "--")
-    return gs, {}
-    
-def mitigation_comparison(fig, data, x_col, hue_col, ylim=(0,1), y_label=None, x_label=None, style_col=None, style_dict={}, color_dict={}, mean_col='Mean', lower_CI_col="lower_CI", upper_CI_col="upper_CI", compare_title=None):
-    gs = fig.add_gridspec(2,3)
+ 
+def mitigation_comparison(
+    fig, data, x_col, s_col, hue_col, ylim=(0,1), y_label=None, x_label=None, style_col=None, style_dict={}, color_dict={}, 
+    mean_col='Mean', lower_CI_col="lower_CI", upper_CI_col="upper_CI", plot_section=[], compare_title=None):
+    row_num = ceil(len(plot_section) / 3)
+    gs = fig.add_gridspec(row_num,3)
     data = data.sort_values(x_col)
     axes = []
-    for r in range(2):
+    for r in range(row_num):
       for c in range(3):
-        axes.append(fig.add_subplot(gs[r,c]))
-        axes[-1].set_ylim(ylim)
-        axes[-1].set_xticks(data[x_col].unique().tolist())
-        if x_label:
-          axes[-1].set_xlabel(x_label)
-        if c == 0:
-          axes[-1].set_ylabel(y_label)
-        else:
-          axes[-1].set_yticklabels([])
-    mitigation_plots = ["(i) No Mitigation", "(ii) Image Cropping", "legends", "(iii) Reweighing", "(iv) Calibrated\nEqualized Odds", "(v) Reject Option\nBased Classification"]
+        if (r * 3 + c + 1) <= len(plot_section):
+            axes.append(fig.add_subplot(gs[r,c]))
+            axes[-1].set_ylim(ylim)
+            axes[-1].set_xticks(data[x_col].unique().tolist())
+            if x_label:
+              axes[-1].set_xlabel(x_label)
+            if c == 0:
+              axes[-1].set_ylabel(y_label)
+            else:
+              axes[-1].set_yticklabels([])
+    mitigation_plots = plot_section
     for i, m in enumerate(mitigation_plots):
       ax = axes[i]
       ax.set_title(m)
@@ -82,7 +71,7 @@ def mitigation_comparison(fig, data, x_col, hue_col, ylim=(0,1), y_label=None, x
           style_legend = ax.legend(handles=style_lines, title="Positive-Associated", bbox_to_anchor=(0.5,0.5), loc='upper center')
           fig.add_artist(hue_legend)
       else:
-        temp_data = data[data['Mitigation'] == m.split(") ")[-1].replace("\nBased","").replace("\n"," ")].copy()
+        temp_data = data[data[s_col] == m].copy()
         if len(temp_data) == 0:
           ax.text((ax.get_xlim()[1] - ax.get_xlim()[0])/2 + ax.get_xlim()[0], (ax.get_ylim()[1] - ax.get_ylim()[0])/2 + ax.get_ylim()[0], "N/A", va='center', ha='center', bbox=dict(facecolor='white', edgecolor='white'), fontsize=16)
           continue
@@ -106,68 +95,29 @@ def mitigation_comparison(fig, data, x_col, hue_col, ylim=(0,1), y_label=None, x
         ax.set_xticklabels(labels)
     return gs, dict(w_pad=0.1, h_pad=0.75)
     
-def no_mitigation_plot(fig, data, x_col, hue_col, ylim=(0,1), figsize=(8,6), y_label=None, x_label=None, style_col=None, style_dict={}, color_dict={}, mean_col='Mean', lower_CI_col="lower_CI", upper_CI_col="upper_CI", compare_title=None, title=None):
-    gs = fig.add_gridspec(1,1)
-    ax = fig.add_subplot(gs[0,0])
-    ax.set_ylim(ylim)
-    data = data[data['Mitigation'] == "No Mitigation"].sort_values(x_col).copy()
-    ax.set_title(title)
-    ax.set_ylabel(y_label)
-    ax.set_xlabel(x_label)
-    if style_col is not None:
-      gb = [hue_col, style_col]
-    else:
-      gb = [hue_col]
-    for gp, df in data.groupby(gb):
-      hue = gp[0]
-      if style_col:
-        style = gp[-1]
-      else:
-        style = 'Default'
-      
-      ax.fill_between(df[x_col], df[lower_CI_col], df[upper_CI_col], color=color_dict[hue], alpha=CI_ALPHA)
-      ax.plot(df[x_col], df[mean_col], c=color_dict[hue], ls=style_dict[style])
-    ax.set_xticks(data[x_col].unique().tolist())
-    # set the "0" to "B" (for baseline)
-    labels = ax.get_xticks().tolist()
-    labels[0] = "B"
-    ax.set_xticklabels(labels)
-    
-    name_map = SUBGROUP_NAME_MAPPING
-    for h in data[hue_col].unique().tolist():
-        if h not in name_map:
-            name_map[h] = h
-    hue_lines = [Patch(facecolor=color_dict[h], label=name_map[h]) for h in data[hue_col].unique().tolist()]
-    hue_legend = ax.legend(handles=hue_lines,  title=compare_title, loc='upper left')
-    if style_col:
-      style_lines = [Line2D([0], [0], ls=style_dict[s], color='k', label=name_map[s]) for s in data[style_col].unique().tolist()]
-      style_legend = ax.legend(handles=style_lines, title="Positive-Associated", loc='upper right')
-      fig.add_artist(hue_legend)
-    
-    return gs, {}
-    
 
-def calculate_CI(df, mean_col='mean', std_col='std', confidence_level=0.95, sample_size=25):
+def calculate_CI(df, mean_col='Mean', std_col='Std', confidence_level=0.95, sample_size=25):
     z_stats = {0.90:1.64, 0.95:1.96, 0.99:2.57}
     z = z_stats[confidence_level]
-    df['lower_CI'] = df['Mean'] - z*(df['Std'] / (sample_size**(0.5)) )
-    df['upper_CI'] = df['Mean'] + z*(df['Std'] / (sample_size**(0.5)) )
+    df['lower_CI'] = df[mean_col] - z*(df[std_col] / (sample_size**(0.5)) )
+    df['upper_CI'] = df[mean_col] + z*(df[std_col] / (sample_size**(0.5)) )
     return df
-
-FUNCTION_DICT = {"sampler": color_style_sampler, "compare":mitigation_comparison, "baseline": no_mitigation_plot}
 
 def result_plotting(variables, csv_path, exp_type, study_type):
     data = pd.read_csv(csv_path)  
     
     
     plot_kwargs = dict(style_col=variables.get('Positive-associated Subgroup'), mean_col=variables.get('Metric Mean Value'), color_dict=COLORS, style_dict=STYLES)  
-    #if study_type == 'Compare Bias Mitigation Methods':
-    #    s_col = variables.get('Mitigation Method')
-    #    section_name = data[s_col].unique()
-    #elif study_type == 'Study Finite Sample Size Effect':
-    #    s_col = variables.get('Training Data Size')
-    #    section_name = data[s_col].unique()
-
+    if study_type == 'Compare Bias Mitigation Methods':
+        s_col = variables.get('Mitigation Method')
+        section_name = data[s_col].unique().tolist()
+    elif study_type == 'Study Finite Sample Size Effect':
+        s_col = variables.get('Training Data Size')
+        section_name = data[s_col].unique().tolist()
+        section_name.sort()
+    section_name.insert(0, "legends")
+    plot_kwargs['s_col'] = s_col
+    plot_kwargs['plot_section'] = section_name
     if exp_type == 'Quantitative Misrepresentation':        
         x_col = variables.get('Training Prevalence Difference')
         degree_name = "Training Prevalence Difference (%)"
@@ -184,12 +134,11 @@ def result_plotting(variables, csv_path, exp_type, study_type):
     info_list = []
     for i, m in enumerate(m_list):
         temp_data = data[(data[m_col] == m)].copy()
-        if m == 'AUROC':
-            kwargs = dict(ylim=(0.5,0.9), y_label=m)
-        elif m == 'Prevalence':
-            kwargs = dict(ylim=(0,1), y_label="Predicted\nPrevalence (%)")
+        kwargs = dict(y_label=m)
         kwargs['compare_title'] = 'Population'
-        fig = plot_wrapper("compare", data=temp_data.copy(), x_label=degree_name, hue_col='Subgroup', **kwargs, **plot_kwargs)
+        fig = plt.figure(figsize = (text_width, text_width*0.75))
+        gs, gs_kwargs = mitigation_comparison(fig, data=temp_data.copy(), x_label=degree_name, hue_col='Subgroup', **kwargs, **plot_kwargs)
+        gs.tight_layout(fig, **gs_kwargs)
         fig.savefig(os.path.join('../example/', f"example_{i}.png"), bbox_inches='tight')
         plt.close("all")
         info = f"The report shows the results of {study_type.lower()} experiment \nwhen bias is amplified by {exp_type.lower()}.\n"
