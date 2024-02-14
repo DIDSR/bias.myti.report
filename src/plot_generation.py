15706cc1d07539654ebe5449a196d08537606e72
yuhangzhang0819 <Yuhang.Zhang@fda.hhs.gov>
2024-02-14 16:18:40 -0500

    Modifying the code structure. Cleaning up the code. Adding documentation.

--- src/plot_generation.py
+++ src/plot_generation.py
@@ -8,7 +8,7 @@ from math import ceil
 import os
 
 # Formatting details
-text_width = 8 # in inches - doesn't have to be exact, just consistent
+text_width = 8 # in inches
 COLORS = {"F":"#dd337c", "M":"#0fb5ae", "Overall":"#57c44f", "B":"#4046ca", "W":"#f68511", "COVID":"#57c44f", "Subgroup":"#7e84fa",} 
 STYLES = {"Default":"-", "M":"--", "F":"-", "B":"-", "W":"--"} # positive-associated
 ACCENT_COLOR = "#430c82" # used for bias deg indicator arrow and accompanying text
@@ -16,6 +16,7 @@ HIGHLIGHT_COLOR = "#FDCA40"
 SUBGROUP_NAME_MAPPING = {"F":"Female", "M":"Male","B":"Black", "W":"White"} # for the legend(s)
 CI_ALPHA = 0.3
 AXIS_COLOR = "#6e6e6e"
+NUM_SUB_COL = 3 # number of plots in a row in the figure
 
 # Matplotlib Rc parameters (importing this file will apply them)
 rcParams['axes.labelweight'] = 'bold'
@@ -32,16 +33,53 @@ rcParams['ytick.color'] = AXIS_COLOR
 rcParams['axes.labelcolor'] = AXIS_COLOR
 rcParams['axes.edgecolor'] = AXIS_COLOR
  
-def mitigation_comparison(
-    fig, data, x_col, s_col, hue_col, ylim=(0,1), y_label=None, x_label=None, style_col=None, style_dict={}, color_dict={}, 
-    mean_col='Mean', lower_CI_col="lower_CI", upper_CI_col="upper_CI", plot_section=[], compare_title=None):
-    row_num = ceil(len(plot_section) / 3)
-    gs = fig.add_gridspec(row_num,3)
+def figure_plotting(
+    data, x_col, s_col, hue_col, ylim=(0,1), y_label=None, x_label=None, style_col=None, style_dict={}, color_dict={}, 
+    mean_col='Mean', lower_CI_col="lower_CI", upper_CI_col="upper_CI", plot_section=[]):  
+    """
+    Function to generate subplots with input plot sections and parameters.
+
+    Arguments
+    =========
+    data
+        dataframe that contains the data for plotting
+    x_col
+        name for column that contains x-axis ticks
+    s_col
+        name for column that contains sub-sections in the figure
+    hue_col
+        name for column that contains subgroups mapped with different colors during plotting
+    ylim
+        set the y-limits for all axes
+    y_label
+        set the y label name
+    x_label
+        set the x label name
+    style_col
+        name for column that determine line styles by positive-associated subgroup
+    style_dict
+        dictionary that determines plotting style
+    color_dict
+        dictionary that determins plotting colors
+    mean_col
+        name for column that contains metric mean value
+    lower_CI_col
+        name for column that contains lower bound of confidence interval
+    upper_CI_col
+        name for column that contains upper bound of confidence interval
+    plot_section
+        list that has all the sub-sections for plotting (including legends section)
+    
+    """
+    row_num = ceil(len(plot_section) / NUM_SUB_COL) #calculate reguired number of rows
+    # # create figure with sub-sections
+    fig = plt.figure(figsize = (text_width, text_width*0.75))
+    gs = fig.add_gridspec(row_num,NUM_SUB_COL)
     data = data.sort_values(x_col)
     axes = []
     for r in range(row_num):
-      for c in range(3):
-        if (r * 3 + c + 1) <= len(plot_section):
+      for c in range(NUM_SUB_COL):
+        if (r * NUM_SUB_COL + c + 1) <= len(plot_section):
             axes.append(fig.add_subplot(gs[r,c]))
             axes[-1].set_ylim(ylim)
             axes[-1].set_xticks(data[x_col].unique().tolist())
@@ -51,10 +89,11 @@ def mitigation_comparison(
               axes[-1].set_ylabel(y_label)
             else:
               axes[-1].set_yticklabels([])
-    mitigation_plots = plot_section
-    for i, m in enumerate(mitigation_plots):
+    # # generate plots in each sub-sections
+    for i, m in enumerate(plot_section):
       ax = axes[i]
       ax.set_title(m)
+      # # plot legends in an individual sub-section
       if m == "legends":
         ax.axis("off")
         ax.set_title(None)
@@ -65,14 +104,15 @@ def mitigation_comparison(
             if h not in name_map:
                 name_map[h] = h
         hue_lines = [Patch(facecolor=color_dict[h], label=name_map[h]) for h in data[hue_col].unique().tolist()]
-        hue_legend = ax.legend(handles=hue_lines, bbox_to_anchor=(0.5,0.5), loc='lower center', title=compare_title)
+        hue_legend = ax.legend(handles=hue_lines, bbox_to_anchor=(0.5,0.5), loc='lower center', title=hue_col)
         if style_col:
           style_lines = [Line2D([0], [0], ls=style_dict[s], color='k', label=name_map[s]) for s in data[style_col].unique().tolist()]
           style_legend = ax.legend(handles=style_lines, title="Positive-Associated", bbox_to_anchor=(0.5,0.5), loc='upper center')
           fig.add_artist(hue_legend)
+      # # generate plots from the input data
       else:
         temp_data = data[data[s_col] == m].copy()
-        if len(temp_data) == 0:
+        if len(temp_data) == 0: # blank plot if no available data
           ax.text((ax.get_xlim()[1] - ax.get_xlim()[0])/2 + ax.get_xlim()[0], (ax.get_ylim()[1] - ax.get_ylim()[0])/2 + ax.get_ylim()[0], "N/A", va='center', ha='center', bbox=dict(facecolor='white', edgecolor='white'), fontsize=16)
           continue
         if style_col is not None:
@@ -85,29 +125,73 @@ def mitigation_comparison(
             style = gp[-1]
           else:
             style = 'Default'
-          
+          # plot line and confidence interval
           ax.fill_between(df[x_col], df[lower_CI_col], df[upper_CI_col], color=color_dict[hue], alpha=CI_ALPHA)
           ax.plot(df[x_col], df[mean_col], c=color_dict[hue], ls=style_dict[style])
         ax.set_xticks(data[x_col].unique().tolist())
         # set the "0" to "B" (for baseline)
         labels = ax.get_xticks().tolist()
         labels[0] = "B"
-        ax.set_xticklabels(labels)
-    return gs, dict(w_pad=0.1, h_pad=0.75)
+        ax.set_xticklabels(labels)    
+    # # save the figure
+    gs.tight_layout(fig, w_pad=0.1, h_pad=0.75)
+    fig.savefig(os.path.join('../example/', f"figure_{y_label}.png"), bbox_inches='tight')
+    plt.close("all")
     
 
 def calculate_CI(df, mean_col='Mean', std_col='Std', confidence_level=0.95, sample_size=25):
+    """
+    Function to calculate confidence interval according to standard deviation, confidence level and sample size.
+    Arguments
+    =========
+    df
+        dataframe that contains the data to calculate CI
+    mean_col
+        name for column with mean value
+    std_col
+        name for column with standard deviation
+    confidence_level
+        confidence level, currently support 0.9, 0.95 and 0.99
+    sample size
+        number of samples for each experiments in the data
+
+    Returns
+    =======
+    pandas.DataFrame
+        dataframe that adds computed lower and upper confidence interval bound
+    """
     z_stats = {0.90:1.64, 0.95:1.96, 0.99:2.57}
     z = z_stats[confidence_level]
     df['lower_CI'] = df[mean_col] - z*(df[std_col] / (sample_size**(0.5)) )
     df['upper_CI'] = df[mean_col] + z*(df[std_col] / (sample_size**(0.5)) )
     return df
 
-def result_plotting(variables, csv_path, exp_type, study_type):
-    data = pd.read_csv(csv_path)  
-    
-    
-    plot_kwargs = dict(style_col=variables.get('Positive-associated Subgroup'), mean_col=variables.get('Metric Mean Value'), color_dict=COLORS, style_dict=STYLES)  
+def bias_report_generation(variables, csv_path, exp_type, study_type):
+    """
+    Function to load inputs from the user, generate report figures and descriptions.
+
+    Arguments
+    =========
+    variables
+        dictionary that contains user specified column names for every variables
+    csv_path
+        path for the csv file which contains the data
+    exp_type
+        string to indicate the bias amplification type
+    study_type
+        string to indicate the study type
+
+    Returns
+    =======
+    m_list : obj:`list`
+        list contains plotted metrics
+    info_list : obj:`list`
+        list contains report description text corresponding to each metric
+    """
+    data = pd.read_csv(csv_path)
+    # # get user input positive-associated group and metric value column for plotting         
+    plot_kwargs = dict(style_col=variables.get('Positive-associated Subgroup'), hue_col=variables.get('Subgroup'), mean_col=variables.get('Metric Mean Value'), color_dict=COLORS, style_dict=STYLES)  
+    # # get study type to establish sub-sections
     if study_type == 'Compare Bias Mitigation Methods':
         s_col = variables.get('Mitigation Method')
         section_name = data[s_col].unique().tolist()
@@ -115,9 +199,13 @@ def result_plotting(variables, csv_path, exp_type, study_type):
         s_col = variables.get('Training Data Size')
         section_name = data[s_col].unique().tolist()
         section_name.sort()
+    else:
+        raise NotImplementedError()
+    # # add a separate section to plot legends
     section_name.insert(0, "legends")
     plot_kwargs['s_col'] = s_col
     plot_kwargs['plot_section'] = section_name
+    # # get bias amplification type
     if exp_type == 'Quantitative Misrepresentation':        
         x_col = variables.get('Training Prevalence Difference')
         degree_name = "Training Prevalence Difference (%)"
@@ -126,21 +214,20 @@ def result_plotting(variables, csv_path, exp_type, study_type):
         degree_name = "Frozen Layers"
     else:
         raise NotImplementedError()
-    plot_kwargs["x_col"] = x_col 
+    plot_kwargs["x_col"] = x_col
+    plot_kwargs["x_label"] = degree_name
+    # # calculate confidence interval 
     data = calculate_CI(data, mean_col=variables.get('Metric Mean Value'), std_col=variables.get('Metric Standard Deviation'))
-
+    # # get list of metrics for plotting
     m_col = variables.get('Metric Name')
     m_list = list(data[m_col].unique())
     info_list = []
+    # # plot each metric in a loop
     for i, m in enumerate(m_list):
-        temp_data = data[(data[m_col] == m)].copy()
-        kwargs = dict(y_label=m)
-        kwargs['compare_title'] = 'Population'
-        fig = plt.figure(figsize = (text_width, text_width*0.75))
-        gs, gs_kwargs = mitigation_comparison(fig, data=temp_data.copy(), x_label=degree_name, hue_col='Subgroup', **kwargs, **plot_kwargs)
-        gs.tight_layout(fig, **gs_kwargs)
-        fig.savefig(os.path.join('../example/', f"example_{i}.png"), bbox_inches='tight')
-        plt.close("all")
+        temp_data = data[(data[m_col] == m)].copy() # data for current metric
+        # # plotting
+        figure_plotting(data=temp_data.copy(), y_label=m, **plot_kwargs)        
+        # # generate corresponding report description
         info = f"The report shows the results of {study_type.lower()} experiment \nwhen bias is amplified by {exp_type.lower()}.\n"
         if study_type == "Compare Bias Mitigation Methods":
             info = info + f"Subplots in the above figure are comparing bias mitigation methods with baseline.\n"
@@ -158,19 +245,34 @@ def result_plotting(variables, csv_path, exp_type, study_type):
     
 
 def save_report(info, img_path, save_path):
-    info = info + "\n\nReport generated by: myti.report v1.0" + f"\nDate: {date.today()}"
+    """
+    Function to combine the figure and report description, add title and other information, and save the report.
+
+    Arguments
+    =========
+    info
+        report description text
+    img_path
+        path for the figure which being saved
+    save_path
+        path to save the final report
+    """
     fig = plt.figure(figsize = (text_width, text_width))
     gs = fig.add_gridspec(4,1)
     ax1 = fig.add_subplot(gs[:-1,:])
-    images = plt.imread(img_path)
-    ax1.axis("off")
+    # # add title
     ax1.set_title('Bias Amplification/Mitigation Report')
+    # # add figure
+    images = plt.imread(img_path)
+    ax1.axis("off")    
     ax1.imshow(images)
+    # # add report description
     ax2 = fig.add_subplot(gs[-1,:])
     ax2.axis("off")
     ax2.set_title(None)
     ax2.set_ylim(0,1)
     ax2.set_xlim(0,1)
+    info = info + "\n\nReport generated by: myti.report v1.0" + f"\nDate: {date.today()}"
     ax2.text(0.02, 0.25, info)
     logo = plt.imread('UI_assets/fda_logo.jpg')
     fig.figimage(logo, 1700, 10)
