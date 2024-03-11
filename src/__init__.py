d9ab641e0010964a828c8eb8494abe87a404f8fb
Burgon <alburgon2@gmail.com>
2024-03-11 14:38:06 -0400

    added workaround for import issue in jupyter notebooks

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
-from .mytiUI import ClickLabel, Page, InitialPage, SecondPage, FinalPage, MainWindow, AboutWindow
+try:
+    from .mytiUI import ClickLabel, Page, InitialPage, SecondPage, FinalPage, MainWindow, AboutWindow
+except:
+    pass
 from .plot_generation import figure_plotting, calculate_CI, bias_plots_generation
 from .report_generation import create_report, on_page
